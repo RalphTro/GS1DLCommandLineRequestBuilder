ba2671d7660b89d0279ec0ea343c30cf94143a88
RalphTro <46958354+RalphTro@users.noreply.github.com>
2024-05-31 15:42:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GS1DLCommandLineRequestBuilder
+ GS1 Digital Link Command Line Request Builder
---
+++ js/main.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+
+	$("#gtininput").on("mousedown", function () {
+		$("#gtininput").val('');
+	});
+
+	$("#gtininput").on("keydown", function (evt) {
+		let key = evt.which;
+		if (key < 48 || key > 57) {
+			alert('Please enter digits only');
+		}
+	});
+
+	$("#resolverinput").on("mousedown", function () {
+		$("#resolverinput").val('');
+	});
+
+	$("#linkset").on("mousedown", function () {
+		checkResolverURL();
+		checkGTINFormat();
+		buildLinksetRequest();
+	});
+
+	$("#default").on("mousedown", function () {
+		checkResolverURL();
+		checkGTINFormat();
+		buildDefaultRequest();
+	});
+
+	$("#specific").on("mousedown", function () {
+		checkResolverURL();
+		checkGTINFormat();
+		addLinkType();
+		addCurlParams();
+		buildSpecificRequest();
+	});
+
+	$("#clipboard").on("mousedown", function () {
+		copyToClipboard();
+	});
+
+	$('#clipboardnote').hide();
+	$('#gtinhint').hide();
+	$('#urlhint').hide();
+
+});
+
+function checkResolverURL() {
+	let resolverURL = $("#resolverinput").val();
+	if (!resolverURL.startsWith('http') || !resolverURL.endsWith('/')) {
+		$('#urlhint').show();
+		$('#urlhint').fadeOut(3000);
+	}
+};
+
+function checkGTINFormat() {
+	let GTIN = $("#gtininput").val();
+	if (/^\d{14}$/.test(GTIN) == false) {
+		$('#gtinhint').show();
+		$('#gtinhint').fadeOut(3000);
+	};
+};
+
+function buildDefaultRequest() {
+	let curl = 'curl -L -X GET ' + $("#resolverinput").val() + '01/' + $("#gtininput").val() + revocationParam;
+	$("#curl").html(curl);
+};
+
+function buildLinksetRequest() {
+	let curl = 'curl -L -X GET ' + $("#resolverinput").val() + '01/' + $("#gtininput").val() + '?linkType=linkset -H "Accept: application/linkset+json"' + revocationParam;
+	$("#curl").html(curl);
+};
+
+function buildSpecificRequest() {
+	let curl = 'curl -L -X GET ' + $("#resolverinput").val() + '01/' + $("#gtininput").val() + 
+	'?linkType=' + linkType + ' ' + curlParams + revocationParam;
+	$("#curl").html(curl);
+};
+
+function copyToClipboard(value) {
+	let textToCopy = $("#curl").html();
+	navigator.clipboard.writeText(textToCopy)
+		.then(() => {
+			$('#clipboardnote').show();
+			$('#clipboardnote').fadeOut(3000);
+		});
+};
+
+let revocationParam = "";
+$("#revocation").click(function() {
+	if ($(this).is(':checked')) {
+		revocationParam = "";
+	} else {
+		revocationParam += " --ssl-no-revoke";
+	}
+});
+
+let linkType = "";
+function addLinkType() {
+	if ($('#linktype').val() != null) {
+		linkType = $('#linktype').val();
+	} else {
+		linkType = 'gs1:defaultLink';
+	};
+};
+
+let curlParams = "";
+function addCurlParams() {
+	if ($('#mimetype').val() != null) {
+		curlParams += '-H "Accept: ' + $('#mimetype').val() + '"';
+	};
+	if ($('#language').val() != null) {
+		curlParams += ' -H "Accept-language: ' + $('#language').val() + '"';
+	};
+};
+
